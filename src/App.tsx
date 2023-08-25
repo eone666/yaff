7ffb7dabea831bf6365024445c470d8651255923
Ivan Avdeyev <eone.ukg@gmail.com>
2023-08-25 20:13:51 +0600

    index page

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import classes from "./App.module.css";
+
+export default function App () {
+    return <div class={classes.root}>
+        <h1>YAFF: Yet Another Frontend Framework</h1>
+        <h2>Purpose: Just For Fun</h2>
+        <div>Check it on <a href="https://github.com/eone666/yaff">GitHub</a>!</div>
+    </div>
+}
