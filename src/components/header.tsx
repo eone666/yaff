49cdcb03dd6647d10ceaaa78c2a6dd38543695d1
Ivan Avdeyev <eone.ukg@gmail.com>
2023-08-31 14:17:12 +0600

    remove link from logo
    TODO: add link after adding routing

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import Link from "./link.tsx";
 export function Header(){
     return <header className="border-b border-gray-500 h-[50px]">
         <Container className="h-full gap-[8px] flex justify-between items-center">
-            <Link href="/" className="font-extrabold text-2xl">YAFF</Link>
+            <span className="font-extrabold text-2xl">YAFF</span>
             <span> Check it on <Link className="hover:no-underline underline" href="https://github.com/eone666/yaff">Github</Link></span>
         </Container>
     </header>
